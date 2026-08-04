10bf4ede618383717c8cc90f0c20d34951d4db2b
manueninfotech <manuenit@outlook.in>
2026-08-04 17:23:36 +0530

    backend render configuration

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { createContact, getContacts } from "../controllers/contactController.js";
+import { createContact } from "../controllers/contactController.js";
+// import { getContacts } from "../controllers/contactController.js";
 
 const router = express.Router();
 
 router.post("/contact", upload.single("file"), createContact);
-router.get("/contacts", getContacts);
+//router.get("/contacts", getContacts);
 
 export default router;
