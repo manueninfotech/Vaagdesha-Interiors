a1131009f28cc34441b6c928a57284955d817f32
manueninfotech <manuenit@outlook.in>
2026-08-05 11:27:39 +0530

    Use Gmail SMTP on port 587

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -4,10 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4, // force IPv4
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
