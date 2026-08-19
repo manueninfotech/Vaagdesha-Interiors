6f0858597c77f67b7baf378c361f64511fc5a9bd
manueninfotech <manuenit@outlook.in>
2026-08-19 16:50:06 +0530

    SEO Changes for Vaagdesha

--- frontend/src/components/SEO.jsx
+++ frontend/src/components/SEO.jsx
@@ -4,11 +4,10 @@ export default function SEO({
   title,
   description,
   url,
-  noindex = false,
+  keywords,
 }) {
   return (
     <Helmet>
-      {/* Basic SEO */}
       <title>{title}</title>
 
       <meta
@@ -16,24 +15,9 @@ export default function SEO({
         content={description}
       />
 
-      {/* Canonical URL */}
-      {url && (
-        <link
-          rel="canonical"
-          href={url}
-        />
-      )}
-
-      {/* Robots */}
       <meta
-        name="robots"
-        content={noindex ? "noindex, nofollow" : "index, follow"}
-      />
-
-      {/* Open Graph */}
-      <meta
-        property="og:type"
-        content="website"
+        name="keywords"
+        content={keywords}
       />
 
       <meta
@@ -45,38 +29,10 @@ export default function SEO({
         property="og:description"
         content={description}
       />
-
-      {url && (
-        <meta
-          property="og:url"
-          content={url}
-        />
-      )}
-
+      
       <meta
-        property="og:site_name"
-        content="Vaagdeesha Interiors"
-      />
-
-      {/* Twitter / X */}
-      <meta
-        name="twitter:card"
-        content="summary_large_image"
-      />
-
-      <meta
-        name="twitter:title"
-        content={title}
-      />
-
-      <meta
-        name="twitter:description"
-        content={description}
-      />
-
-      <meta
-        name="twitter:image"
-        content={image}
+        property="og:url"
+        content={url}
       />
     </Helmet>
   );
